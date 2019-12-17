080928a1e8492ba4104c451458a8af6bd3d4cb1d
AlexanderFaican <roberth.faican@unl.edu.ec>
2019-12-17 11:54:56 -0500

    Documentación

---
+++ nodeadmin/functions/documentation/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "PROJECTRK",  "version": "0.0.0",  "description": "API REST service",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-12-17T16:53:45.492Z",    "url": "http://apidocjs.com",    "version": "0.18.0"  }});
